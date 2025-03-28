63c442ab1df714fad83f783aac0e83f5b9d5d528
JavierVentur <j72562869@gmail.com>
2025-03-28 09:17:08 -0600

    Create me.js
    Aqui se una carpeta llamada Controller y en el ella un file llamado me.js

---
+++ server/controllers/me.js
@@ -0,0 +1,47 @@
+const conexion =require('../database/db');
+
+exports.save=(req,res)=>{
+    const nombre=(req.body.nombre);
+    const apellido=(req.body.apellido);
+    const edad=(req.body.edad);
+    const telefono=(req.body.telefono);
+    const cuidad=(req.body.cuidad);
+    
+    conexion.query('insert into clientes set ?', {nombre:nombre,apellido:apellido,edad:edad,
+    telefono:telefono,cuidad:cuidad},(error,resultado=>{
+        if(error){
+            console.log(error);
+        }
+        else{
+            res.redirect('/cliente2')
+        }
+
+
+    }
+    
+    ));
+
+
+}
+
+exports.edit=(req,res)=>{
+    const codigo=(req.body.codigo);
+    const nombre=(req.body.nombre);
+    const apellido=(req.body.apellido);
+    const edad=(req.body.edad);
+    const telefono=(req.body.telefono);
+    const cuidad=(req.body.cuidad);
+    
+    conexion.query("update clientes set  ?  where codigo =?",[{nombre:nombre,apellido:apellido,edad:edad,
+        telefono:telefono,cuidad:cuidad},codigo],(error,resultado)=>{
+            if(error){
+                console.log(error);
+            }
+            else{
+                res.redirect('/cliente2')
+            }
+
+    });
+    
+
+};
