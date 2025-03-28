c3f9a3f4c1ee56d9ea24d900685e747cde1758c9
JavierVentur <j72562869@gmail.com>
2025-03-28 09:08:51 -0600

    Update router.js

---
+++ server/router.js
@@ -0,0 +1,76 @@
+const express=require('express');
+const router=express.Router();
+
+const conexion=require('./database/db');
+
+router.get('/Cliente',(req,res)=>{
+    conexion.query("select * from clientes",(error,resultado)=>{
+            if(error){
+                console.log(error);
+                return;
+            }
+            else{
+            res.send(resultado);
+            }
+    });
+});
+
+router.get('/Cliente2',(req,res)=>{
+    conexion.query("select * from clientes",(error,resultado)=>{
+            if(error){
+                console.log(error);
+                return;
+            }
+            else{
+           // res.send(resultado);
+            //let hola="hola y adios";
+            res.render('cliente/index',{clientes:resultado});
+            }
+    });
+});
+
+//Ruta para llenar formulario de crear cliente
+router.get('/crear',(req,res)=>{
+    
+  res.render('cliente/crear');
+});
+
+
+
+const metodos=require('./controllers/me');
+router.post('/save',metodos.save);
+
+
+
+router.get('/editar/:id',(req,res)=>{
+const codigo=req.params.id;
+conexion.query("select * from clientes where codigo = ?",[codigo],(error,resultado)=>{
+    if (error){
+        console.log(error);
+        return
+    }
+    else{
+        res.render('cliente/editar',{clientes:resultado}); }
+        
+
+    });    
+
+
+  });
+  router.post('/edit',metodos.edit);
+
+
+
+router.get('/Empleados',(req,res)=>{
+res.send('Esta la ruta de Empleados');
+});
+router.get('/Empleados2',(req,res)=>{
+res.render('empleado/index');
+});
+
+
+
+
+
+
+module.exports=router;
