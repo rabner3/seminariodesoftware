352887dd99d67ba8e82950489ad410ab9282b367
llagosbanadesa <llagos@banadesa.hn>
2025-04-04 15:11:04 -0600

    correccion usuarios

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -22,35 +22,38 @@ const Layout = ({ children, user }) => {
 
       {/* Barra lateral */}
       <header>
-        <div className="logo-container">
-          <Link to="/" title="Inicio">
-            <img src="/img/logo-proyecto.png" alt="logo" />
-          </Link>
-        </div>
+        <div>
+          <div className="logo-container">
+            <Link to="/" title="Inicio">
+              <img src="/img/logo-proyecto.png" alt="logo" />
+            </Link>
+          </div>
 
-        <nav>
-          <ul>
-            <li>
-              <Link to="/usuarios" title="Usuarios">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960">
-                  <path d="M309-390h342v-25q0-43-45.5-68.5T480-509q-80 0-125.5 25.5T309-415v25Zm170.88-185Q514-575 537-597.38q23-22.37 23-56.5Q560-688 537.12-711q-22.87-23-57-23Q446-734 423-711.12q-23 22.87-23 57 0 34.12 22.88 56.62 22.87 22.5 57 22.5ZM215-215l-79 79q-23 23-52 11.31T55-168v-643q0-39.46 27.47-67.23Q109.95-906 149-906h662q39.46 0 67.23 27.77Q906-850.46 906-811v502q0 39.05-27.77 66.52Q850.46-215 811-215H215Z" />
-                </svg>
-              </Link>
-            </li>
-          </ul>
-        </nav>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/usuarios" title="Usuarios">
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960">
+                    <path d="M15-243q0-42 21.5-76T97-371q75-32 136.5-47T360-433q67 0 127.5 14.5T623-371q39 17 61 51t22 77v27q0 39-27.5 66.5T611-122H109q-39 0-66.5-27.5T15-216v-27Zm721 121q13-22 21.5-45t8.5-49v-21q0-68-29-116t-98-82q64 9 126.5 25T866-375q36 19 57.5 55t21.5 82v22q0 39-27.5 66.5T851-122H736ZM360-494q-72 0-117-45t-45-118q0-72 45-117t117-45q72 0 117.5 45T523-657q0 73-45.5 118T360-494Zm397-163q0 72-45.5 117.5T594-494q-12 0-24.5-2t-26.5-7q26-27 38.5-66t12.5-88q0-48-12.5-85.5T543-810q12-4 25-6.5t26-2.5q72 0 117.5 45.5T757-657Z" />
+                  </svg>
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
 
         <Link className="log-out" to="/logout" title="Salir">
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
             <path d="M502.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-128-128c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L402.7 224 192 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l210.7 0-73.4 73.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l128-128zM160 96c17.7 0 32-14.3 32-32s-14.3-32-32-32L96 32C43 32 0 75 0 128L0 384c0 53 43 96 96 96l64 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-64 0c-17.7 0-32-14.3-32-32l0-256c0-17.7 14.3-32 32-32l64 0z" />
           </svg>
         </Link>
-      </header>
+
+      </header >
 
       {/* Contenido dinámico */}
-      <main>
+      < main >
         <div id="main-container" className="main-container">{children}</div>
-      </main>
+      </main >
     </div>
   );
 };
--- client/src/pages/Usuarios.jsx
+++ client/src/pages/Usuarios.jsx
@@ -1,4 +1,4 @@
-function Home() {
+function Usuarios() {
   return <div class="contenedor-padre" id="contenedor-padre">
     <h1>Pagina de Usuarios</h1>
   </div>;
